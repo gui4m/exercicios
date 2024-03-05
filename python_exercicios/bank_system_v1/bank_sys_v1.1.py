576f6f0bfb6bfaf24f63151d8b2a9d279756b89d
Guilherme Amaral <45568072+gui4m@users.noreply.github.com>
2024-03-05 11:55:16 -0300

    Update and rename bank_sys_v1.py to bank_sys_v1.1.py

--- python_exercicios/bank_system_v1/bank_sys_v1.1.py
+++ python_exercicios/bank_system_v1/bank_sys_v1.1.py
@@ -17,8 +17,8 @@
     * Exibir valores no formato "R$ xxx.xx".
 '''
 
+# Define the menu string to display available bank operations
 menu = '''
-
 === BANCO LOREM IPSUM ===
 
       Depositar [d]
@@ -29,40 +29,53 @@ menu = '''
 Digite a letra correspondente à Operação desejada.
 => '''
 
+# Initialize the account balance, withdrawal limit, transaction history, and daily withdrawal count
 saldo = 0
 limite = 500
 extrato = ''
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-
+# Main loop to handle user operations
 while True:
+    # Get user input for the operation they want to perform
     opcao = input(menu)
 
+    # Deposit operation
     if opcao == "d":
+        # Initialize deposit amount
         deposito = 0
         
+        # Display deposit header
         print("\n\n {:^30}".format("DEPÓSITO") + f"\n{'='*30}")
+        # Get deposit amount from user
         deposito = float(input("Digite o valor do Depósito: "))
+        # Validate deposit amount
         if deposito <= 0:
             print("ERRO: Valor Inválido! Digite um valor positivo.")
-            opcao = "d"
+        
         else:
+            # Update account balance and transaction history
             saldo += deposito
             extrato = extrato + (f'Depósito: R$ {deposito:.2f} \n')
             print("Operação Realizada com Sucesso!")
     
+    # Withdrawal operation
     elif opcao == "s":
         saque = 0
         numero_saques += 1
 
+        # Display withdrawal header
         print("\n\n {:^30}".format("SAQUE") + f"\n{'='*30}")
         
+        # Check if the number of withdrawals has reached the limit
         if numero_saques > LIMITE_SAQUES:
             print("Alerta: Número de SAQUES DIÁRIOS já atingiu o limite!")
 
         else:
+            # Get withdrawal amount from user
             saque = float(input("Digite o valor do saque: "))
+            # Validate withdrawal amount
             if saque > 500:
                 print(f"ERRO: Valor de saque por operação excedido! Digite um valor de até R${limite}.")
                 numero_saques -= 1
@@ -76,17 +89,21 @@ while True:
                 numero_saques -= 1
         
             else:
+                # Update account balance and transaction history
                 saldo -= saque
                 extrato = extrato + (f'Saque: R$ {saque:.2f} \n')
                 print("Operação Realizada com Sucesso!")
                 
+    # Display account statement
     elif opcao == "e":
         print("\n\n {:^20}".format("Extrato") + f"\n{20*'='}")
         print(f"{extrato}{20*'='}\n Saldo: R$ {saldo:.2f}")
 
+    # Exit the program
     elif opcao == "q":
         print("Obrigado por ser nosso cliente!")
         break
 
+    # Invalid operation
     else:
         print("Operação INVÁLIDA! Por favor selecione novamente a operação desejada ")
